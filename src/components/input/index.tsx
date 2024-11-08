4a1e87c0d343876754e6085dabe98a3447a39f58
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-11-08 13:37:52 -0300

    refactor: modify input component props

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,22 +1,20 @@
 import { TextInput, TextInputProps, View, ActivityIndicator } from "react-native";
 import { styles } from "./styles";
-import { useFonts, PasseroOne_400Regular } from '@expo-google-fonts/passero-one';
+import { forwardRef } from "react";
 
 
-export default function Input({placeholder, secureTextEntry} : TextInputProps) {
-    const [fontsLoaded] = useFonts({
-        PasseroOne_400Regular,
-    });
-    if (!fontsLoaded) {
-        return <ActivityIndicator size="small" color="#000" />; // Carregando enquanto a fonte não está disponível
-    }
+function Input({...rest} : TextInputProps, ref: React.Ref<TextInput>) {
+    
     return (
         <View style = {styles.inputContainer}>
             <TextInput
                 style={[styles.input, { fontFamily: 'PasseroOne_400Regular' }]}
-                placeholder={placeholder}
-                secureTextEntry={secureTextEntry}
+                ref={ref}
+                placeholder={rest.placeholder}
+                {...rest}
             />
         </View>
     )
 }
+
+export default forwardRef(Input)
--- src/components/input/styles.tsx
+++ src/components/input/styles.tsx
@@ -6,9 +6,11 @@ export const styles = StyleSheet.create({
   inputContainer: {
     borderColor: "black",
     borderWidth: 2,
+    borderRadius: 5,
     justifyContent: "center",
-    width: width * 0.70,
-    height: height * 0.08,
+    width: width * 0.7,
+    height: height * 0.07,
+    margin: '3%'
   },
   input: {
     margin: 5,
