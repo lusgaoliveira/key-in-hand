9348e2aab73ecdc8a253e59594c694cb4deaa25f
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-11-09 10:45:30 -0300

    style: add theme config

---
+++ theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+    light: {
+        background: "#B9D6F2",
+        placeholder: "#B0B0B0",
+        text: "#FFFFFF",
+        moveForward: "#061A40",
+        stepBack: "#0353A4",
+    },
+    dark: { },
+}
