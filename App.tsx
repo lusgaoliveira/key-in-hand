fc30909591dc43aa4775790b8c609e17f5e6449d
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-11-09 10:46:25 -0300

    feat: add loginscreen on app

--- App.tsx
+++ App.tsx
@@ -1,25 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import Input from './src/components/input';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import LoginScreen from './src/screens/login';
+import { useEffect } from 'react';
 
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    'Title': require('./assets/fonts/Text.ttf'), 
+    'Text': require('./assets/fonts/Text.ttf'),   
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+  // Verificação inicial: enquanto as fontes não estão carregadas e não há erro, retorna 'null' para não renderizar nada
+  if (!loaded && !error) {
+    return null;
+  }
+
+  // Renderiza a navegação principal do aplicativo após o carregamento das fontes
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Input
-        placeholder='username'
-        secureTextEntry={false}
-      />
-    </View>
+    <LoginScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
