26960ed5142841c3611b245093d39c8e93a3e569
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-11-25 00:58:07 -0300

    refactor: change some css proprities

--- theme.ts
+++ theme.ts
@@ -2,7 +2,7 @@ export const theme = {
     light: {
         background: "#B9D6F2",
         placeholder: "#B0B0B0",
-        text: "#FFFFFF",
+        text: "black",
         moveForward: "#061A40",
         stepBack: "#0353A4",
     },
