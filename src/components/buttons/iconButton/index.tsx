7f3cd590e190153a19c77095d95a2083b43dcc10
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-11-25 00:48:32 -0300

    feat: add icon button

---
+++ src/components/buttons/iconButton/index.tsx
@@ -0,0 +1,20 @@
+import { Pressable, PressableProps, StyleProp, Text, View, ViewStyle } from "react-native";
+import styles from "./styles";
+import { FontAwesome } from "@expo/vector-icons";
+
+type IconsKeys = 'edit' | 'plus-circle' | 'search' | 'trash' | 'copy';
+
+type ButtonProps = PressableProps & {
+    icon: IconsKeys;
+    style?: StyleProp<ViewStyle>;
+};
+
+export default function IconButton({ icon, style, ...rest} : ButtonProps) {
+    return (
+        <View style={[styles.container, style]}>
+            <Pressable style={styles.button} {...rest}>
+                <FontAwesome name={icon} size={26}/>
+            </Pressable>
+        </View>
+    )
+}
---
+++ src/components/buttons/iconButton/styles.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        width: '10%',
+        height: '5%',
+        paddingVertical: 10
+    },
+    button: {
+        width: '100%',             
+        height: 45,                
+        borderRadius: 25,          
+        alignItems: 'center',      
+        justifyContent: 'center',  
+    },
+})
+
+export default styles;
