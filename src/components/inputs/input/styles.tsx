712f5adbfc59ee4bc729f53e7a948d4795913681
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-11-30 01:15:06 -0300

    style: modify style of input to login screen

--- src/components/inputs/input/styles.tsx
+++ src/components/inputs/input/styles.tsx
@@ -7,14 +7,14 @@ export const styles = StyleSheet.create({
     borderWidth: 2,
     borderRadius: 5,
     justifyContent: "center",
-    width: width * 0.7,
-    height: height * 0.07,
+    width: '70%',
+    height: 'auto',
     margin: '3%'
   },
   input: {
     margin: '3%',
     textAlign: "left",
-    fontSize: 32,
+    fontSize: 22,
     fontWeight: 'bold',
     color: 'black'
   },
