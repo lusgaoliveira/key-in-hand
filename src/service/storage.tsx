e1631cb3299f934ce5b84a23ff70fcf9e6e951d2
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-12-09 23:17:37 -0300

    feat: add all basics functions to storage

--- src/service/storage.tsx
+++ src/service/storage.tsx
@@ -1,13 +1,26 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const STORAGE_KEY = '@keys';
+
 export const saveKey = async (key: string, data: object) => {
-    try {
-        const existingKeys = await AsyncStorage.getItem("@keys");
-        const keys = existingKeys ? JSON.parse(existingKeys) : {};
-        keys[key] = data; // Salvar o objeto com a chave única
-        await AsyncStorage.setItem("@keys", JSON.stringify(keys));
-    } catch (error) {
-        console.error("Error saving key to storage:", error);
-        throw error;
-    }
+    const keys = JSON.parse((await AsyncStorage.getItem(STORAGE_KEY)) || '{}');
+    keys[key] = data;
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(keys));
+};
+
+export const getKeys = async () => {
+    return JSON.parse((await AsyncStorage.getItem(STORAGE_KEY)) || '{}');
+};
+
+export const getKey = async (key: string) => {
+    const keysInData = await AsyncStorage.getItem(STORAGE_KEY);
+    const keys = keysInData ? JSON.parse(keysInData) : {};
+    return keys[key] || null;
+};
+
+export const deleteKey = async (key: string) => {
+    const keysInData = await AsyncStorage.getItem(STORAGE_KEY);
+    const keys = keysInData ? JSON.parse(keysInData) : {};
+    delete keys[key];
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(keys));
 };
