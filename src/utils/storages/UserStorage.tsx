80a0c03fb73b1669831c3dc4a44daf4aa5602d28
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2024-12-13 21:41:02 -0300

    fix: resolve error on user storage save and get

--- src/utils/storages/UserStorage.tsx
+++ src/utils/storages/UserStorage.tsx
@@ -3,15 +3,21 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 export class UserStorage {
     static STORAGE_USER = '@users';
 
-    static async saveUser(username : string, data : object) {
-        const users = JSON.parse(await AsyncStorage.getItem(this.STORAGE_USER) || '{}');
-        users[username] = data;
-        await AsyncStorage.setItem(this.STORAGE_USER, JSON.stringify(users));
+    static async saveUser(username: string, data: object): Promise<boolean> {
+        try {
+            const users = JSON.parse(await AsyncStorage.getItem(this.STORAGE_USER) || '{}');
+            users[username] = data;
+            await AsyncStorage.setItem(this.STORAGE_USER, JSON.stringify(users)); 
+            console.log(data);
+            return true;
+        } catch (error) {
+            console.error('Erro on save/edit user::', error);
+            return false;
+        }
     }
-
-    static async getUser(username : string) {
+    static async getUser(username: string) {
         const users = JSON.parse(await AsyncStorage.getItem(this.STORAGE_USER) || '{}');
-        return users[username] || null;
+        return users[username] || null; 
     }
 
     static async getAllUsers() {
